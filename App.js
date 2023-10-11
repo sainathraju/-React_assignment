36090f56f4f1130285754af08979827cb311a8ff
sainathraju <147332852+sainathraju@users.noreply.github.com>
2023-10-11 20:53:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+var parent=[React.createElement("div",{},React.createElement("h1",{},"Nested_Element")),
+   React.createElement("h3",{},"Hello World")]
+
+var htmlRoot=document.getElementById("root")
+
+var root=ReactDOM.createRoot(htmlRoot)
+
+root.render(parent)
